400dfef50cc33b30c0642e95e011928285c1b667
Jordan <jordanroberts@talk21.com>
2019-04-26 09:40:19 +0100

    First passing feature test - user is prompted to enter name and date of birth

--- app.rb
+++ app.rb
@@ -3,10 +3,11 @@ require 'sinatra/base'
 class BirthdayGreeter < Sinatra::Base
 
   get '/' do
-    'Hello'
     erb :index
   end
 
+  post '/calculator' do
+  end
   run! if app_file == $0
 
 end
--- spec/features/entering_dob_spec.rb
+++ spec/features/entering_dob_spec.rb
@@ -1,8 +1,10 @@
 feature 'user visits home page' do
   it 'prompts a user to enter name and date of birth' do
     visit('/')
-    fill_in 'dob', with:
+    fill_in 'name', with: 'Jordan'
+    select '1', from: 'day'
+    select 'February', from: 'month'
     click_button 'Submit'
-    expect(page).to have_content ''
+    #expect(page).to have_content 
   end
 end
