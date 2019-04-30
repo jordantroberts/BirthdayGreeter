28fed851ffebfe29a02099efff718bc33caafc2c
Jordan <jordanroberts@talk21.com>
2019-04-30 07:51:55 +0100

    Extracted logic to controller and greeting based on conditionals to the view. I deleted the Greeter class for the moment as I wasn't using it. The app works as intended but needs testing

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
-require './lib/greeter'
+require 'date'
+
 
 class BirthdayGreeter < Sinatra::Base
 
@@ -11,9 +12,37 @@ class BirthdayGreeter < Sinatra::Base
     @name = params[:name]
     @day = params[:day]
     @month = params[:month]
-    @greeter = Greeter.new
+
+    monthnums = {
+    "January" => 1,
+    "February" => 2,
+    "March" => 3,
+    "April" => 4,
+    "May" => 5,
+    "June" => 6,
+    "July" => 7,
+    "August" => 8,
+    "September" => 9,
+    "October" => 10,
+    "November" => 11,
+    "December" => 12
+  }
+    birthday_date = Date.new(Date.today.year, monthnums[@month], @day.to_i)
+    date_today = Date.today
+    @diff = (date_today - birthday_date).to_i
+    # 
+    # if @diff == 0
+    #   puts "Happy birthday #{@name}"
+    # elsif @diff > 0
+    #   puts "You have #{365 - @diff} days until your Birthday"
+    #   @diff = 365 - @diff
+    # else
+    #   @diff = @diff.abs
+    #   puts "You have #{365 - @diff} days until your Birthday"
+    # end
 
     erb :greeting
+
   end
 
   run! if app_file == $0
--- spec/greeter_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'greeter'
-
-describe '.greeter' do
-  describe '#match' do
-    it 'calculates whether your birthday' do
-
-    end
-  end
-end 
